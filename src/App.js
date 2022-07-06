685d58a2f8490d6e926f053ceb10b5687cb31b2e
Reimirno <long_yu@berkeley.edu>
2022-07-06 16:31:37 -0700

    feat: add and setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Homepage />;
+  return (
+    <Routes>
+      <Route exact path="/" element={<Homepage />} />
+      <Route exact path="/about" element={<Homepage />} />
+    </Routes>
+  );
 }
 
 export default App;
