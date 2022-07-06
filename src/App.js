51a38617c3e96d054c503a7f4ecb2d7327f2aea5
Reimirno <long_yu@berkeley.edu>
2022-07-06 16:40:47 -0700

    feat: add dummy about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
-import Homepage from "./pages/homepage/homepage.jsx";
+import Homepage from "./pages/homepage/homepage";
+import AboutPage from "./pages/aboutpage/aboutpage";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Homepage />} />
-      <Route exact path="/about" element={<Homepage />} />
+      <Route exact path="/about" element={<AboutPage />} />
     </Routes>
   );
 }
