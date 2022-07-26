094116f3146272e60037cf314cdc7fb03a8afc26
Yu Long <long_yu@berkeley.edu>
2022-07-25 22:18:40 -0700

    refactor: move directory info into redux state

--- src/components/menu-directory/menu-directory.jsx
+++ src/components/menu-directory/menu-directory.jsx
@@ -1,24 +1,20 @@
 import React from "react";
-import MENU_DATA from "../../data/menu.data";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectDirectorySections } from "../../redux/directory/directory.selector";
 import MenuItem from "../menu-item/menu-item";
 import "./menu-directory.scss";
 
-class MenuDirectory extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      sections: MENU_DATA,
-    };
-  }
-  render() {
-    return (
-      <div className="menu-directory">
-        {this.state.sections.map(({ id, ...other }) => (
-          <MenuItem key={id} {...other} desc="SHOP NOW" />
-        ))}
-      </div>
-    );
-  }
-}
+const MenuDirectory = ({ sections }) => (
+  <div className="menu-directory">
+    {sections.map(({ id, ...other }) => (
+      <MenuItem key={id} {...other} desc="SHOP NOW" />
+    ))}
+  </div>
+);
 
-export default MenuDirectory;
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
+
+export default connect(mapStateToProps)(MenuDirectory);
