a511e922d37e58b221d56011f7433e12b349e727
Yu Long <long_yu@berkeley.edu>
2022-07-25 22:39:21 -0700

    refactor: move shop page state into redux

--- src/pages/shoppage/shoppage.jsx
+++ src/pages/shoppage/shoppage.jsx
@@ -1,26 +1,19 @@
 import "./shoppage.scss";
-import React from "react";
-import SHOP_DATA from "../../data/shop.data";
 import CollectionPrewiew from "../../components/collection-preview/collection-preview";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectShopCollections } from "../../redux/shop/shop.selector";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...other }) => (
+      <CollectionPrewiew key={id} {...other} />
+    ))}
+  </div>
+);
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...other }) => (
-          <CollectionPrewiew key={id} {...other} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections,
+});
 
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
