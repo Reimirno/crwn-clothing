92e6ca06363f919e047aca3a349466bb9feb13ff
Yu Long <long_yu@berkeley.edu>
2022-07-25 21:20:06 -0700

    feat: add redux-persist to persist cart across sessions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 // import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,7 +13,9 @@ root.render(
   <Provider store={store}>
     <React.StrictMode>
       <Router>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Router>
     </React.StrictMode>
   </Provider>
