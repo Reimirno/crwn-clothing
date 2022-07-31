32dfc12169e179eb16d47d81d383d28ffaff5a91
Yu Long <long_yu@berkeley.edu>
2022-07-30 21:31:07 -0700

    fix: update test visa card payment info

--- src/pages/checkoutpage/checkoutpage.jsx
+++ src/pages/checkoutpage/checkoutpage.jsx
@@ -25,7 +25,7 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className="test-warning">
         *Please use the following test credit card for payments!"
         <br />
-        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+        4242 4242 4242 4242 - Exp: 12/30 - CVV: 123
       </div>
       <StripeCheckoutButton priceInDollar={total} />
     </div>
